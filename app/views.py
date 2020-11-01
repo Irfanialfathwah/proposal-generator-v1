b5fb818820c0bafee63b088eaf7a35c0a18d7a06
Irfany Al Fathwah <irfanialfathwah@gmail.com>
2020-11-01 10:19:13 +0700

    Updating Route For Login

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 from app import app
 
@@ -8,9 +8,15 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/administrator')
+@app.route('/admin', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again!'
+        else:
+            return redirect(url_for('index'))
+    return render_template("login.html", error=error)
 
 
 @app.route('/about')
