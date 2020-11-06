e8f84eb4ff2acb07802c06b73913caf2eb039c1d
hadyanadam <hadyanadamn@gmail.com>
2020-11-06 08:44:33 +0700

    reset

--- app/models.py
+++ app/models.py
@@ -6,12 +6,29 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(100))
     email = db.Column(db.String(30), unique=True, nullable=False)
     # role = db.Column(db.String(30), nullable=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
 
 # class Role(db.Model):
 #     __tablename__ = "roles"
