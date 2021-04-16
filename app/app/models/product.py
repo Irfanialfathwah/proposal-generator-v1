f15fe260d7c8bd7f3ffaf6bddfaea2da2a4b0ef7
Irfany Al Fathwah <irfanialfathwah@gmail.com>
2021-04-16 13:57:02 +0700

    add brochure to product table

--- app/app/models/product.py
+++ app/app/models/product.py
@@ -7,12 +7,15 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(100), nullable=True)
     std_price = db.Column(db.Integer, nullable=True)
+    brochure = db.Column(db.String(100), nullable=True)
     proposals = db.relationship('Proposal', backref="product", cascade="all,delete")
     created_at = db.Column(db.DateTime, nullable=False)
     updated_at = db.Column(db.DateTime, nullable=False)
 
-    def update(self, product_name, std_price):
+    def update(self, product_name, std_price, brochure=None):
         timestamp = datetime.now().replace(microsecond=0)
         self.product_name = product_name
         self.std_price = std_price
+        if brochure is not None:
+            self.brochure = brochure
         self.updated_at = timestamp
